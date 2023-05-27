7e95054b00c5cd22d280be87c85c038fd49e7150
Yasiru Deshan <rpyasiru@gmail.com>
2023-05-27 16:12:34 +0530

    basic environment done

---
+++ app/index.js
@@ -0,0 +1,11 @@
+import {View, Text} from 'react-native'
+
+const Home = () => {
+    return (
+        <View>
+            <Text>Yasiru Deshan</Text>
+        </View>
+    )
+}
+
+export default Home
